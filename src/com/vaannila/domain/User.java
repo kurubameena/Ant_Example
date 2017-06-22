dfb43e0e9d3cfd1d7a5f9692cd9e59e44f4c5b03
kurubameena <meenasreekumari42@gmail.com>
2017-06-22 18:44:08 +0530

    add age for user  file

--- src/com/vaannila/domain/User.java
+++ src/com/vaannila/domain/User.java
@@ -6,6 +6,7 @@ public class User {
 	private String password;
 	private String gender;
 	private String country;
+	private String age;
 	private String state;
 	private String aboutYou;
 	private String[] community;
